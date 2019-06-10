eefd54e50f58b267798dbaf789aac3f85338c011
kirstenwerner <kirstenmwerner@gmail.com>
2019-06-10 15:42:03 -0700

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,23 @@
 class ProductsController < ApplicationController
+   before_action :set_product, only: [:data, :show, :description, :inventory]
+
+   def data
+      render json: ProductSerializer.serialize(@product)
+   end
+
   def index
     @products = Product.all
   end
 
+  def show
+  end
+
   def inventory
-    product = Product.find(params[:id])
-    render plain: product.inventory > 0 ? true : false
+    render plain: @product.inventory > 0 ? true : false
   end
 
   def description
-    product = Product.find(params[:id])
-    render plain: product.description
+    render plain: @product.description
   end
 
   def new
@@ -26,5 +33,9 @@ class ProductsController < ApplicationController
 
   def product_params
     params.require(:product).permit(:name, :description, :inventory, :price)
+  end
+
+  def set_product
+      @product = Product.find_by_id(params[:id])
   end
 end
